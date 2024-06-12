f8a6a8d300eb6b7007807c820d408d899c5c6e36
xakyth <xakyth@gmail.com>
2024-06-12 10:14:32 +0300

    prettier format

--- server.js
+++ server.js
@@ -3,25 +3,27 @@ const app = express();
 
 const PORT = 3000;
 
-app.use(express.static('public'))
-app.use(express.json())
+app.use(express.static('public'));
+app.use(express.json());
 
-const addon = require('bindings')('addon.node')
+const addon = require('bindings')('addon.node');
 const privileges = {
-  0: "Гость",
-  1: "Пользователь",
-  2: "Администратор"
-}
+  0: 'Гость',
+  1: 'Пользователь',
+  2: 'Администратор',
+};
 
 app.post('/checkUser', (req, res) => {
   const { username } = req.body;
   const checkResult = addon.checkPrivileges(username);
-  if (checkResult == -1) {
+  if (checkResult === -1) {
     return res.send(`Пользователя ${username} нет`);
   }
-  res.send(`Пользователь ${username} имеет привилегию ${privileges[checkResult]}`);
-})
+  res.send(
+    `Пользователь ${username} имеет привилегию ${privileges[checkResult]}`
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
